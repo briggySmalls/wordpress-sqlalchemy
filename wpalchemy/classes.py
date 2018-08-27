aac337dd95616fe4fd97c72a4f36adb09dea392b
briggySmalls <wasso14@hotmail.com>
2018-08-28 00:06:09 +0100

    Total overhaul to use declarative syntax

--- wpalchemy/classes.py
+++ wpalchemy/classes.py
@@ -1,218 +1,138 @@
-import sqlalchemy.orm as orm
-from wpalchemy import tables
+from sqlalchemy import Column, Integer, String, ForeignKey, Text, DateTime, UniqueConstraint
+from sqlalchemy.ext.declarative import declarative_base
 
-metadata = tables.metadata
+Base = declarative_base()
 
-class Term(object):
-    def __init__(self, name, slug, term_group=0):
-        self.name = name
-        self.slug = slug
-        self.term_group = term_group
 
-    def __repr__(self):
-        return '<Term(%r, %r, %r)>' % (self.name, self.slug, self.term_group)
+class Comments(Base):
+    __tablename__ = 'wp_comments'
+    comment_ID = Column(Integer, primary_key=True)
+    comment_post_ID = Column(Integer, ForeignKey('wp_posts.ID'))
+    comment_author = Column(Text(length=None))
+    comment_author_email = Column(String(length=100))
+    comment_author_url = Column(String(length=200))
+    comment_author_IP = Column(String(length=100))
+    comment_date = Column(DateTime(timezone=False))
+    comment_date_gmt = Column(DateTime(timezone=False))
+    comment_content = Column(Text(length=None))
+    comment_karma = Column(Integer)
+    comment_approved = Column(String(length=4))
+    comment_agent = Column(String(length=255))
+    comment_type = Column(String(length=20))
+    comment_parent = Column(Integer, ForeignKey('wp_comments.comment_ID'))
+    user_id = Column(Integer, ForeignKey('wp_users.ID'))
 
-class Taxonomy(object):
-    def __init__(self, term, description):
-        self.term = term
-        self.description = description
 
-class PostTag(Taxonomy):
-    def __repr__(self):
-        return '<PostTag(%r, %r)>' % (self.term, self.description)
+class Links(Base):
+    __tablename__ = 'wp_links'
+    link_id = Column(Integer, primary_key=True)
+    link_url = Column(String(length=255))
+    link_name = Column(String(length=255))
+    link_image = Column(String(length=255))
+    link_target = Column(String(length=25))
+    link_description = Column(String(length=255))
+    link_visible = Column(String(length=1))
+    link_owner = Column(Integer, ForeignKey('wp_users.ID'))
+    link_rating = Column(Integer)
+    link_updated = Column(DateTime(timezone=False))
+    link_rel = Column(String(length=255))
+    link_notes = Column(Text(length=None))
+    link_rss = Column(String(length=255))
 
-class Category(Taxonomy):
-    def __repr__(self):
-        return '<Category(%r, %r)>' % (self.term, self.description)
 
-class LinkCategory(Taxonomy):
-    def __repr__(self):
-        return '<LinkCategory(%r, %r)>' % (self.term, self.description)
+class Options(Base):
+    __tablename__ = 'wp_options'
+    option_id = Column(Integer, primary_key=True)
+    blog_id = Column(Integer, primary_key=True)
+    option_name = Column(String(length=64), primary_key=True)
+    option_value = Column(Text(length=None))
+    autoload = Column(String(length=3))
 
-class Post(object):
-    def __init__(self, post_title, post_type='post'):
-        self.post_title = post_title
-        self.post_type = post_type
+class Postmeta(Base):
+    __tablename__ = 'wp_postmeta'
+    meta_id = Column(Integer, primary_key=True)
+    post_id = Column(Integer, ForeignKey('wp_posts.ID'))
+    meta_key = Column(String(length=255), primary_key=False)
+    meta_value = Column(Text(length=None), primary_key=False)
 
-    def __repr__(self):
-        return '<Post(%r, %r)>' % (self.post_title, self.post_type)
 
-class PostMeta(object):
-    def __init__(self, meta_key, meta_value):
-        self.meta_key = meta_key
-        self.meta_value = meta_value
+class Posts(Base):
+    __tablename__ = 'wp_posts'
+    ID = Column(Integer, primary_key=True)
+    post_author = Column(Integer, ForeignKey('wp_users.ID'))
+    post_date = Column(DateTime(timezone=False))
+    post_date_gmt = Column(DateTime(timezone=False))
+    post_content = Column(Text(length=None))
+    post_title = Column(Text(length=None))
+    post_excerpt = Column(Text(length=None))
+    post_status = Column(String(length=10))
+    comment_status = Column(String(length=15))
+    ping_status = Column(String(length=6))
+    post_password = Column(String(length=20))
+    post_name = Column(String(length=200))
+    to_ping = Column(Text(length=None))
+    pinged = Column(Text(length=None))
+    post_modified = Column(DateTime(timezone=False))
+    post_modified_gmt = Column(DateTime(timezone=False))
+    post_content_filtered = Column(Text(length=None))
+    post_parent = Column(Integer, ForeignKey('wp_posts.ID'))
+    guid = Column(String(length=255))
+    menu_order = Column(Integer)
+    post_type = Column(String(length=20))
+    post_mime_type = Column(String(length=100))
+    comment_count = Column(Integer)
 
-    def __repr__(self):
-        return '<PostMeta(%r, %r)>' % (self.meta_key, self.meta_value)
 
-class Link(object):
-    def __init__(self, link_url, link_name):
-        self.link_url = link_url
-        self.link_name = link_name
+# TODO: replace with SQLAlchemy many-to-many construct
+class TermRelationships(Base):
+    __tablename__ = 'wp_term_relationships'
+    object_id = Column(Integer, ForeignKey('wp_post.ID'), primary_key=True)
+    term_taxonomy_id = Column(Integer, ForeignKey('wp_term_taxonomy.term_taxonomy_id'), primary_key=True)
 
-    def __repr__(self):
-        return '<Link(%r, %r)>' % (self.link_url, self.link_name)
 
-class Comment(object):
-    def __init__(self, comment_author, comment_content):
-        self.comment_author = comment_author
-        self.comment_content = comment_content
+class TermTaxonomy(Base):
+    __tablename__ = 'wp_term_taxonomy'
+    term_taxonomy_id = Column(Integer, primary_key=True)
+    term_id = Column(Integer, ForeignKey('wp_terms.term_id'))
+    taxonomy = Column(String(length=32))
+    description = Column(Text(length=None))
+    parent = Column(Integer, ForeignKey('wp_term_taxonomy.term_taxonomy_id'))
+    count = Column(Integer)
 
-    def __repr__(self):
-        return '<Comment(%r, %r)>' % (self.comment_author, self.comment_content)
+    __table_args__ = (
+        UniqueConstraint('term_id', 'taxonomy'),
+    )
 
-class User(object):
-    def __init__(self, user_login):
-        self.user_login = user_login
 
-    def __repr__(self):
-        return '<User(%r)>' % self.user_login
+class Terms(Base):
+    __tablename__ = 'wp_terms'
+    term_id = Column(Integer, primary_key=True)
+    name = Column(String(length=55))
+    slug = Column(String(length=200))
+    term_group = Column(Integer)
 
-class UserMeta(object):
-    def __init__(self, meta_key, meta_value):
-        self.meta_key = meta_key
-        self.meta_value = meta_value
+    __table_args__ = (
+        UniqueConstraint('slug'),
+    )
 
-    def __repr__(self):
-        return '<UserMeta(%r, %r)>' % (self.meta_key, self.meta_value)
 
-class Option(object):
-    def __init__(self, option_name, option_value):
-        self.option_name = option_name
-        self.option_value = option_value
+class Usermeta(Base):
+    __tablename__ = 'wp_usermeta'
+    umeta_id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('wp_users.ID'))
+    meta_key = Column(String(length=255), primary_key=False)
+    meta_value = Column(Text(length=None), primary_key=False)
 
-    def __repr__(self):
-        return '<Option(%r, %r)>' % (self.option_name, self.option_value)
 
-orm.mapper(Term, tables.terms)
-
-taxonomy_mapper = orm.mapper(
-    Taxonomy,
-    tables.term_taxonomy,
-    properties={'term': orm.relation(Term)},
-    polymorphic_on=tables.term_taxonomy.c.taxonomy,
-)
-
-orm.mapper(
-    PostTag,
-    properties={
-        'posts': orm.dynamic_loader(
-            Post,
-            secondary=tables.term_relationships,
-            primaryjoin=(tables.term_taxonomy.c.term_taxonomy_id
-                         == tables.term_relationships.c.term_taxonomy_id),
-            secondaryjoin=(tables.term_relationships.c.object_id
-                           == tables.posts.c.ID),
-            foreign_keys=[tables.term_relationships.c.object_id,
-                          tables.term_relationships.c.term_taxonomy_id],
-        ),
-    },
-    inherits=taxonomy_mapper,
-    polymorphic_identity='post_tag',
-)
-
-orm.mapper(
-    Category,
-    properties={
-        'children': orm.relation(
-            Category,
-            backref=orm.backref('parent_category',
-                                remote_side=[tables.term_taxonomy.c.term_taxonomy_id]),
-        ),
-        'posts': orm.dynamic_loader(
-            Post,
-            secondary=tables.term_relationships,
-            primaryjoin=(tables.term_taxonomy.c.term_taxonomy_id
-                         == tables.term_relationships.c.term_taxonomy_id),
-            secondaryjoin=(tables.term_relationships.c.object_id
-                           == tables.posts.c.ID),
-            foreign_keys=[tables.term_relationships.c.object_id,
-                          tables.term_relationships.c.term_taxonomy_id],
-        ),
-    },
-    inherits=taxonomy_mapper,
-    polymorphic_identity='category',
-)
-
-orm.mapper(
-    LinkCategory,
-    properties={
-        'links': orm.relation(
-            Link,
-            secondary=tables.term_relationships,
-            primaryjoin=(tables.term_taxonomy.c.term_taxonomy_id
-                         == tables.term_relationships.c.term_taxonomy_id),
-            secondaryjoin=(tables.term_relationships.c.object_id
-                           == tables.links.c.link_id),
-            foreign_keys=[tables.term_relationships.c.object_id,
-                          tables.term_relationships.c.term_taxonomy_id],
-        ),
-    },
-    inherits=taxonomy_mapper,
-    polymorphic_identity='link_category',
-)
-
-orm.mapper(
-    Post,
-    tables.posts,
-    properties={
-        'meta': orm.relation(
-            PostMeta,
-            collection_class=orm.collections.column_mapped_collection(tables.postmeta.c.meta_key),
-        ),
-        'children': orm.relation(
-            Post,
-            backref=orm.backref('parent', remote_side=[tables.posts.c.ID]),
-        ),
-        'tags': orm.relation(
-            PostTag,
-            secondary=tables.term_relationships,
-            primaryjoin=(tables.posts.c.ID
-                         == tables.term_relationships.c.object_id),
-            secondaryjoin=(tables.term_relationships.c.term_taxonomy_id
-                           == tables.term_taxonomy.c.term_taxonomy_id),
-            foreign_keys=[tables.term_relationships.c.object_id,
-                          tables.term_relationships.c.term_taxonomy_id],
-        ),
-        'comments': orm.relation(Comment, backref='post'),
-    },
-)
-
-orm.mapper(PostMeta, tables.postmeta)
-
-orm.mapper(
-    Link,
-    tables.links,
-    properties={
-    },
-)
-
-orm.mapper(
-    Comment,
-    tables.comments,
-    properties={
-        'children': orm.relation(
-            Comment,
-            backref=orm.backref('parent',
-                                remote_side=[tables.comments.c.comment_ID]),
-        ),
-    },
-)
-
-orm.mapper(
-    User,
-    tables.users,
-    properties={
-        'meta': orm.relation(
-            UserMeta,
-            collection_class=orm.collections.column_mapped_collection(tables.usermeta.c.meta_key),
-        ),
-        'posts': orm.dynamic_loader(Post, backref='user'),
-        'links': orm.dynamic_loader(Link, backref='user'),
-        'comments': orm.dynamic_loader(Comment, backref='user'),
-    },
-)
-
-orm.mapper(UserMeta, tables.usermeta)
-
-orm.mapper(Option, tables.options)
+class Users(Base):
+    __tablename__ = 'wp_users'
+    ID = Column(Integer, primary_key=True)
+    user_login = Column(String(length=60))
+    user_pass = Column(String(length=64))
+    user_nicename = Column(String(length=50))
+    user_email = Column(String(length=100))
+    user_url = Column(String(length=100))
+    user_registered = Column(DateTime(timezone=False))
+    user_activation_key = Column(String(length=60))
+    user_status = Column(Integer)
+    display_name = Column(String(length=250))
